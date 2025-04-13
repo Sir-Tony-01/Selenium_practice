2c918fc47086e317ea71e85a18055e7b8d9bec8c
Sir-Tony-01 <150789495+Sir-Tony-01@users.noreply.github.com>
2025-04-13 17:44:35 +0600

    Удалены тесты для шаблона проекта

--- tests/test_lockout_user.py
+++
@@ -1,9 +0,0 @@
-from pages.login_page import LoginPage
-
-def test_locked_out_user_cannot_login(driver):
-    login_page = LoginPage(driver)
-    login_page.open()
-    login_page.login("locked_out_user", "secret_sauce")
-
-    error_message = login_page.get_error_message()
-    assert "locked out" in error_message.lower(), f"Unexpected error: {error_message}"
